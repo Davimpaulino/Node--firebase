f87041e9c8eb84ad549eb6caf5934ae80d25417a
davimpaulino <davimpaulino@gmail.com>
2026-09-10 11:25:34 -0300

    att firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,20 +1,11 @@
 const admin = require("firebase-admin");
-let serviceAccount = require("./serviceAccountKey.json");
 
-// Se o JSON veio formatado do arquivo/env com \n como texto puro, converte em quebras de linha reais
-if (typeof serviceAccount === "string") {
-  serviceAccount = JSON.parse(serviceAccount);
-}
-
-if (serviceAccount.private_key) {
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-}
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://sistemasweb-8fc66-default-rtdb.firebaseio.com"
 });
 
-const db = admin.database();
-
-module.exports = db;
+module.exports = admin.database();
